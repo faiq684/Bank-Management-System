3fa2ab73b1e72643a6658be30235f30d98b56d7e
Faiq <faiqspall1@gmail.com>
2025-07-27 22:32:58 +0500

    update

--- bank_ms.py
+++ bank_ms.py
@@ -1,20 +1,20 @@
 usernames = ["Faiq", "Ali", "Zain", "Hadi", "Ibrahim", "Faizan", "Moiz"]
 passwords = ["Faiq1234", "Ali12345", "Zain1234", "Hadi12345", "Ibrahim123", "Faizan123", "Moiz1234"]
 account_numbers = [11111111, 22222222, 33333333, 44444444, 55555555, 6666666, 7777777]
-account_balances = [1000,2000,3000,4000,5000,6000,7000]
+account_balances = [100000,200000,300000,400000,500000,600000,700000]
 
 
-print("\nWelcome to Bank Alfalah Official Website")
+
+print("Welcome to Bank Alfalah Official Mobile App")
 print("\n1: Create a New Account")
-print("\n2: Already have an existing account, Select to Login")
+print("2: Already have an existing account, Select to Login")
 
+login_or_signup_input = input("Enter Your Choice (1 or 2): ")
 
-
-login_or_signup_input = input("Enter Your Choice : (1 or 2)")
 if login_or_signup_input == "1":
-    name_input1 = input("Enter Your First Name :")
-    name_input2 = input("Enter Your Last Name :")  
-    
+    name_input1 = input("Enter Your First Name: ")
+    name_input2 = input("Enter Your Last Name: ")
+
     def check_password_strength(password):
         if len(password) < 8:
             return "Weak: Password should contain at least 8 characters"
@@ -30,22 +30,28 @@ if login_or_signup_input == "1":
         if strength_result == "Strong Password!":
             break
         print(strength_result)
-    
-    
-    cnic_input = input('Enter Your CNIC Number : ')        
-                                             
-if len(cnic_input) != 13:
-        print("Invalid CNIC Number")
-else:
-        phone_number_input = input('Enter Your Phone Number :')
-        if len(phone_number_input) != 11:
-            print("Invalid Phone Number")
+
+    while True:
+        cnic_input = input("Enter Your CNIC Number (without dashes): ")
+        if len(cnic_input) != 13 or not cnic_input.isdigit():
+            print("Invalid CNIC Number - must be 13 digits")
         else:
-         email_input = input("Enter Your Email Address :")
-         print("Account Creation Successfull!")
-        #  update_username_info = name_input1 + name_input2
-        #  update_login_info = usernames.add(update_username_info)
-        #  update_password_info = passwords.add(password_input)
+            break
+
+    while True:
+        phone_number_input = input("Enter Your Phone Number: ")
+        if len(phone_number_input) != 11 or not phone_number_input.isdigit():
+            print("Invalid Phone Number - must be 11 digits")
+        else:
+            break
+
+    email_input = input("Enter Your Email Address: ")
+    print("Account Creation Successful!!")
+    full_name = name_input1 + "_" + name_input2
+    usernames.append(full_name)
+    passwords.append(password_input)
+
+        
          
          
 
@@ -56,9 +62,36 @@ if login_or_signup_input == "2":
         index = usernames.index(username_input)
         if user_password_input == passwords[index]:
             print("Login Successfull !")
+            print(f"Welcome {username_input} ")
             
             balance_inquiry = print("1: Balance inquiry")
             transfer_money = print("2: Transfer Money")
+            deposit_money = print("3: Money Deposit")
+            transaction_choice = input("Enter Your Choice (1-3)")
+            if transaction_choice == "1":
+                
+                print(f"{username_input}, Your Account Balance is {account_balances[index]}")
+            elif transaction_choice == "2":
+                transfer_input = input("Enter the account number of the account you want to transfer money to:")
+                transfer_amount = int(input("Enter the amount you want to transfer: "))
+                if transfer_amount > (account_balances[index]):
+                 print("Insufficient Balance")
+                else:
+                    account_balances[index] -= transfer_amount
+                print(f"Transaction Successful! Your Remaining Balance is {account_balances[index]}")
+
+                 
+                
+                
+                
+                
+                
+            
+                
+            
+            
+            
+            
             
             
             
